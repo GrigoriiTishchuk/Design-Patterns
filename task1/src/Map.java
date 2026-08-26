691ee950006bbd47a3c3bbe2d5b06e65a14bd924
GregTish <tishchuk167@gmail.com>
2026-08-26 09:27:15 +0300

    Map is done

--- task1/src/Map.java
+++ task1/src/Map.java
@@ -1,3 +1,79 @@
-public abstract class Map {
+import java.util.Random;
 
+public abstract class Map {
+    protected Tile[][] grid;
+    protected int width;
+    protected int height;
+
+    public Map(int width, int height) {
+        this.width = width;
+        this.height = height;
+        grid = new Tile[height][width];
+        generateMap();
+    }
+
+    private void generateMap() {
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                grid[y][x] = createTile();
+            }
+        }
+    }
+
+
+    public abstract Tile createTile();
+
+    public void display() {
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                System.out.print(grid[y][x].getCharacter() + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+class CityMap extends Map {
+    public CityMap(int width, int height) {
+        super(width, height);
+    }
+
+    @Override
+    public Tile createTile() {
+        Random rand = new Random();
+        int tileType = rand.nextInt(3);
+        switch (tileType) {
+            case 0:
+                return new RoadTile();
+            case 1:
+                return new ForestTile();
+            case 2:
+                return new BuildingTile();
+            default:
+                return new RoadTile();
+        }
+    }
+}
+
+
+class WildernessMap extends Map {
+    public WildernessMap(int width, int height) {
+        super(width, height);
+    }
+
+    @Override
+    public Tile createTile() {
+        Random rand = new Random();
+        int tileType = rand.nextInt(3);
+        switch (tileType) {
+            case 0:
+                return new WaterTile();
+            case 1:
+                return new ForestTile();
+            case 2:
+                return new SwampTile();
+            default:
+                return new ForestTile();
+        }
+    }
 }
