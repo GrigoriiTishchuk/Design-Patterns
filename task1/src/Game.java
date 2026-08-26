b1d6eafe2393dfc34b266a749bfd2885f1fa907a
GregTish <tishchuk167@gmail.com>
2026-08-26 09:37:49 +0300

    Game is done. Task 1 - Factory Method is done

--- task1/src/Game.java
+++ task1/src/Game.java
@@ -1,2 +1,11 @@
 public class Game {
+    public static void main(String[] args) {
+        Map cityMap = new CityMap(5, 5);
+        System.out.println("City Map:");
+        cityMap.display();
+
+        Map wildernessMap = new WildernessMap(5, 5);
+        System.out.println("\nWilderness Map:");
+        wildernessMap.display();
+    }
 }
